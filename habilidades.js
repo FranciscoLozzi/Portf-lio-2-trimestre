a14c213da28ae3cf5698a04ac8bc5315bccdc3c4
FranciscoLozzi <162700965+FranciscoLozzi@users.noreply.github.com>
2025-09-04 10:49:22 -0300

    Add files via upload

--- habilidades.js
+++ habilidades.js
@@ -1,21 +1,131 @@
-
 const habilidades = {
-  H27: "Aplicar conceitos estatísticos em análise de dados.",
-  H28: "Interpretar gráficos de pizza, moda, média e mediana.",
-  H29: "Organizar dados coletados e apresentar resultados.",
-  H30: "Criar jogos educativos aplicando probabilidade e combinatória.",
-  H31: "Desenvolver roteiros e atividades matemáticas criativas.",
-  H32: "Aplicar princípios de probabilidade em situações práticas."
+
+  Habilidade_nao_informada: "Hablidade não informada",
+  // Habilidades de Matemática
+  
+
+C4mtm: "Analisar e investigar dados e informações de natureza científica e social, articulando diferentes áreas do conhecimento, compreendendo representações gráficas e uso de modelos matemáticos, realizando inferências, previsões e tendências, para tomadas de decisão.",
+  C5mtm: "Aplicar o pensamento probabilístico para quantificar e fazer previsões em situações aplicadas a diferentes áreas do conhecimento e da vida cotidiana.",
+  H27mtm: "Compreender informações estatísticas provenientes de naturezas distintas (social, econômica, política ou científica) e formular juízos com base nessas informações.",
+  H30mtm: "Identificar dados, regularidades e relações numa situação que envolva o raciocínio combinatório, utilizando os processos de contagem, jogos educativos aplicando probabilidade e combinatória.",
+  H31mtm: "Reconhecer fenômenos e eventos (naturais, científicos, tecnológicos e/ou sociais) de caráter aleatório, compreendendo o significado e a importância da probabilidade como meio de prever resultados.",
+  H32mtm: "Identificar em diferentes áreas científicas e outras atividades práticas modelos e problemas que fazem uso de estatísticas e probabilidades.",  // Habilidades de Natureza
+  C1nat: "Compreender as ciências naturais e as tecnologias como construções humanas associadas à cultura dos povos e suas visões de mundo.",
+  H1nat: "Interpretar informações apresentadas em diferentes linguagens usadas nas Ciências, como texto discursivo, gráficos, tabelas, relações matemáticas, diagramas ou representação simbólica.",
+  H3nat: "Inferir significado de termos técnico-científicos em textos de instrumentação ou de divulgação científica.",
+  H4nat: "Identificar, em textos, diagramas, gráficos, imagens e tabelas, informações relevantes para compreender um fenômeno ou conceito relacionado às Ciências da Natureza.",
+
+  C2nat: "Aplicar os conceitos fundamentais e estruturas procedimentais das Ciências da Natureza na explicação de fenômenos cotidianos, bem como dominar processos e práticas da investigação científica.",
+  H6nat: "Compreender os conceitos relacionados à Física em seus diferentes ramos: Astronomia, Mecânica, Acústica, Óptica, Termologia, Calorimetria, Ondulatória Eletricidade, Magnetismo e Física Moderna e Nuclear.",
+  H7nat: "Compreender os conceitos relacionados à Química nos seus diferentes ramos: Fisico-química, Química orgânica e Química Geral.",
+  H9nat: "Explicar os conceitos de energia, matéria, vida e transformação para explicar fenômenos naturais e procedimentos tecnológicos.",
+  H10nat: "Aplicar os conceitos de Física, Química e Biologia de forma integrada na compreensão dos fenômenos naturais.",
+  H11nat: "Empregar procedimentos e práticas de observação, levantamento de hipótese, experimentação, coleta de dados e conclusões para resolução de problemas relacionados às Ciências da Natureza.",
+  H12nat: "Relacionar informações para construir modelos em ciência e tecnologia.",
+
+  C3nat: "Determinar os impactos das ações humanas nos ambientes, identificando suas causas e propondo soluções para a sua redução.",
+  H15nat: "Comparar propostas de intervenção ambiental aplicando conhecimentos científicos e tecnológicos, observando os riscos e benefícios de sua implementação.",
+  H18nat: "Identificar situações de risco ambiental na cidade onde reside.",
+
+  C4nat_comp: "Compreender o funcionamento dos organismos vivos em geral e o ser humano em especial, considerando suas relações com o ambiente em que vivem, abordando aspectos físicos e socioculturais.",
+  H23nat: "Formular propostas de alcance individual ou coletivo, utilizando como critérios a preservação e a promoção da saúde individual e coletiva.",
+
+  C5nat_comp: "Determinar as características das tecnologias associadas às Ciências da Natureza aplicadas em diferentes serviços ou contextos.",
+  H24nat: "Descrever as propriedades físicas, químicas e/ou biológicas dos materiais relacionando-os às finalidades as quais que se destinam.",
+  H25nat: "Identificar matérias-primas e etapas de transformação nos processos.",
+  H26nat: "Utilizar manuais para conhecer os processos de instalação e operação de diferentes equipamentos.",
+  H27nat: "Executar testes de controle para a comparação de serviços, materiais e produtos, utilizando parâmetros como custo e eficiência, empregabilidade, efeito colateral, riscos e benefícios.",
+  H28nat: "Reconhecer e utilizar nomenclatura e códigos científicos e tecnológicos para caracterizar materiais, substâncias e processos.",
+
+  C6nat: "Desenvolver fundamentos técnicos e científicos relativos às grandezas a ao funcionamento de sistemas computacionais, identificando particularidades de desenvolvimento de sistemas, bem como a atuação do técnico no mundo do trabalho.",
+  H34nat: "Interpretar diagramas e esquemas elétricos e de dados.",
+  H35nat: "Interpretar simbologia de componentes eletrônicos e de informação.",
+  H36nat: "Utilizar procedimentos e normas específicos de medição de sinais.",
+
+  // Habilidades de Humanas (adicionadas com sufixo 'hum')
+  C1hum: "Compreender processos históricos sociais e geográficos nos diversos aspectos da vida em sociedade.",
+  H1hum: "Reconhecer o processo de formação do indivíduo nos aspectos históricos, geográficos, sociais e filosóficos.",
+  H2hum: "Compreender o processo de socialização do indivíduo, considerando os princípios do pensamento e da lógica.",
+  H3hum: "Compreender as relações de poder entre os diversos grupos sociais.",
+  H4hum: "Compreender mudanças e permanências ao longo do tempo na constituição da sociedade.",
+  H5hum: "Analisar criticamente os aspectos políticos, econômicos e sociais relacionados à formação da sociedade no Brasil e no mundo.",
+
+  C2hum: "Compreender a importância do trabalho na constituição das sociedades e na formação dos sujeitos.",
+  H6hum: "Identificar as principais características do trabalho humano, considerando suas formas de organização e suas funções em diferentes contextos.",
+  H7hum: "Avaliar o impacto das transformações técnicas e tecnológicas na organização dos processos produtivos do mundo do trabalho.",
+  H8hum: "Analisar as relações entre os agentes envolvidos no mundo do trabalho.",
+  H9hum: "Reconhecer os impactos da divisão do trabalho na realidade socioeconômica e cultural.",
+  H10hum: "Compreender as transformações no mundo do trabalho, geradas por mudanças na ordem econômica.",
+  H11hum: "Comparar diferentes processos de produção e suas implicações histórico-sociais e econômicas.",
+  H12hum: "Propor ações sustentáveis e eficientes visando a melhoria dos processos produtivos.",
+
+  C3hum: "Compreender o ser humano como agente de transformação dos espaços e territórios considerando os aspectos políticos, econômicos e sociais.",
+  H13hum: "Identificar ações de ocupação dos diversos espaços e territórios e os efeitos nas relações do ser humano com a natureza.",
+  H14hum: "Reconhecer o ser humano como agente de transformação dos diversos espaços e territórios e os impactos de suas ações ao meio ambiente.",
+  H15hum: "Reconhecer os movimentos sociais e sua representação da coletividade como elemento de transformação da realidade social, política, econômica e cultural.",
+  H16hum: "Compreender a relação entre ciência, tecnologia e sociedade e os efeitos dessa relação no contexto social, político e econômico.",
+  H17hum: "Avaliar impactos ambientais e sociais decorrentes da utilização de tecnologias na produção e consumo de bens e serviços.",
+  H18hum: "Analisar as relações sociais percebendo as relações de poder e domínios territoriais.",
+  H19hum: "Construir ações inovadoras, visando a gestão de recursos, com eficiência e eficácia, para implementação de tecnologias nos processos produtivos, analisando seus efeitos para a sociedade.",
+  H20hum: "Implementar ações de proteção ou recuperação ambiental com base em princípios, leis e iniciativas de desenvolvimento sustentável.",
+
+  C4hum: "Interpretar dados e informações referentes aos aspectos políticos, econômicos, sociais e culturais da sociedade contemporânea.",
+  H21hum: "Reconhecer manifestações de indivíduos e grupos sociais nos diferentes contextos ao longo do tempo.",
+  H22hum: "Analisar, na perspectiva humana e social, as principais características e dinâmicas dos fluxos da sociedade.",
+  H23hum: "Relacionar textos analíticos e interpretativos sobre diferentes processos histórico-sociais.",
+  H24hum: "Reconhecer o impacto das transformações naturais, sociais, econômicas, culturais e políticas no atual contexto brasileiro.",
+  H25hum: "Interpretar, em fontes orais, visuais, documentais, materiais, informações sobre as dinâmicas sociais ao longo do tempo.",
+  H26hum: "Comparar pontos de vista e ações práticas do indivíduo ou grupo social em diferentes contextos.",
+  H27hum: "Analisar hipóteses e questões ambientais, sociais, econômicas, políticas e culturais, a partir de leituras e debates.",
+
+  C5hum: "Aplicar os fundamentos da cidadania, favorecendo uma atuação consciente do indivíduo na sociedade.",
+  H28hum: "Analisar as diversas fontes de informação e a influência na formação da opinião do indivíduo e na construção da vida em sociedade.",
+  H29hum: "Reconhecer a diversidade cultural entre os indivíduos, compreender e respeitando as diferenças.",
+  H30hum: "Analisar as lutas sociais e conquistas obtidas no que se refere às mudanças nas legislações ou nas políticas públicas.",
+  H31hum: "Analisar a importância da ética e da moral na constituição da sociedade brasileira.",
+  H32hum: "Relacionar ética, moral, cidadania e democracia na organização das sociedades.",
+  H33hum: "Formular estratégias que promovam inclusão social.",
+
+  C6hum: "Avaliar a sociedade como um sistema complexo, estruturado sob os aspectos políticos, econômicos, sociais e geoambientais.",
+  H34hum: "Analisar processos sociais sob a ótica humanizada nas relações pessoais e no mundo do trabalho.",
+  H35hum: "Desenvolver a criticidade sobre o contexto social, político e econômico na sociedade.",
+  H36hum: "Propor interpretações e soluções para problemas identificados no contexto social brasileiro.",
+  H37hum: "Reconhecer características do sistema de organização social, política e econômica.",
+  H38hum: "Compreender os processos histórico-sociais, comparando diferentes explicações sobre fatos e fenômenos.",
+  H39hum: "Relacionar o papel das instituições sociais às soluções para problemas políticos, econômicos, sociais e ambientais no contexto da sociedade brasileira.",
+  H40hum: "Analisar o processo de formação das instituições sociais e políticas, compreendendo seu papel na sociedade.",
+
+  //Habilidades de linguagens
+
+  H6ling: "Habilidade não informada",
+  H8ling: "Habilidade não informada",
+  H4ling: "Habilidade não informada",
+  H14ling: "Habilidade não informada",
+  H17ling: "Habilidade não informada",
+  H23ling: "Habilidade não informada",
+  H22ling: "Habilidade não informada",
+  H11ling: "Habilidade não informada",
+  H15ling: "Habilidade não informada",
+
+  H1ti: "Definir tecnologias de acordo com os requisitos não funcionais.",
+  H2ti: "Integrar sistemas orientados para a conectividade e interoperabilidade.",
+  H3ti: "Reconhecer sistemas de interface para usuários (UX)",
+  H4ti: "Interpretar requisitos levantados para desenvolvimento de sistemas",
+  H5ti: "Habilidade não informada",
+  H6ti: "Reconhecer requisitos de qualidade, integridade, usabiilidade e segurança da informação.",
+  H7ti: "identificar documentação técnica aplicada ao escopo do projeto.",
+  H8ti: "Identificar requisitos funcionais e não-funcionais para desenvolvimento de sistemas."
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   const botoesHab = document.querySelectorAll(".btn-hab");
 
   botoesHab.forEach(btn => {
-    const codigo = btn.textContent.trim();
+    const codigo = btn.dataset.codigo;
     const tooltip = document.createElement("div");
     tooltip.className = "tooltip";
     tooltip.textContent = habilidades[codigo] || "Habilidade não definida";
     btn.parentElement.appendChild(tooltip);
   });
 });
+
